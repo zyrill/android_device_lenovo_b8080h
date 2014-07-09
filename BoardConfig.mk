f6b889c9e482fc4e0bf6d6aa712e0c33d73870cf
zyrill <p.a.c.krueger@gmail.com>
2014-07-10 01:16:30 +0200

    amended BoardConfig with device tree info and set kernel +x

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,24 +1,32 @@
 # BoardConfig.mk
-USE_CAMERA_STUB := true
+#USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
 # -include vendor/LENOVO/B8080/BoardConfigVendor.mk
 
 TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a7
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := MSM8228
+TARGET_BOOTLOADER_BOARD_NAME := B8080
+#TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+#TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+#TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+#TARGET_USES_QCOM_BSP := true
+
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-TARGET_BOOTLOADER_BOARD_NAME := B8080
+#COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_MKBOOTIMG_ARGS := --dt device/LENOVO/B8080/dt.img --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 
 # fix this up by examining /proc/mtd on a running device
 # boot -> /dev/block/mmcblk0p10 fdisk -l /dev/block/mmcblk0p10 16777216 bytes
