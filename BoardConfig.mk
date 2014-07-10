3fb347b10ec52b5365df1172bfe28ee6e08dfc51
zyrill <p.a.c.krueger@gmail.com>
2014-07-10 07:32:12 +0200

    added TARGET_CPU_SMP := true, reorganised BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,35 +4,41 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 # -include vendor/LENOVO/B8080/BoardConfigVendor.mk
 
-TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-TARGET_BOOTLOADER_BOARD_NAME := B8080
-
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_MKBOOTIMG_ARGS := --dt device/LENOVO/B8080/dt.img --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-BOARD_HAS_NO_SELECT_BUTTON := true
 
-# fix this up by examining /proc/mtd on a running device
-# boot -> /dev/block/mmcblk0p10 fdisk -l /dev/block/mmcblk0p10 16777216 bytes
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000 
-# recovery -> /dev/block/mmcblk0p21 fdisk -l /dev/block/mmcblk0p21 16777216 bytes
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01000000
-# system -> /dev/block/mmcblk0p17 fdisk -l /dev/block/mmcblk0p17 1610612736 bytes
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
-# userdata -> /dev/block/mmcblk0p26 fdisk -l /dev/block/mmcblk0p26 12949896704 bytes 
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 12949896704 
+#COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
 
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+#TARGET_BOARD_PLATFORM := MSM8228
+TARGET_BOARD_PLATFORM := unknown
+TARGET_BOOTLOADER_BOARD_NAME := B8080
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a7
+#TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+#TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_NO_BOOTLOADER := true
 TARGET_PREBUILT_KERNEL := device/LENOVO/B8080/kernel
+#TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+#TARGET_USES_QCOM_BSP := true
+
+# boot -> /dev/block/mmcblk0p10 fdisk -l /dev/block/mmcblk0p10 16777216 bytes
+# recovery -> /dev/block/mmcblk0p21 fdisk -l /dev/block/mmcblk0p21 16777216 bytes
+# system -> /dev/block/mmcblk0p17 fdisk -l /dev/block/mmcblk0p17 1610612736 bytes
+# userdata -> /dev/block/mmcblk0p26 fdisk -l /dev/block/mmcblk0p26 12949896704 bytes
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000 
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12949896704 
 
 # TWRP
 DEVICE_RESOLUTION := 1920x1200 
